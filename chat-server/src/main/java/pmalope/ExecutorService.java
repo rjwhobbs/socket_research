b560f6917c3f280fe5ed643e71306fbdac3a9dd8
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-24 19:53:27 +0200

    Is the a difference between 'pool.submit' and 'pool.execute'

--- chat-server/src/main/java/pmalope/ExecutorService.java
+++ chat-server/src/main/java/pmalope/ExecutorService.java
@@ -1,8 +1,11 @@
 package pmalope;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 /*
@@ -11,7 +14,8 @@ import java.util.concurrent.Executors;
 public class ExecutorService {
     public static void main(String[] args) throws IOException {
         System.out.println("Inside: " + Thread.currentThread().getName());
-        new NetworkService(59001, 10).run();
+        NetworkService t = new NetworkService(59001, 10);
+        t.run();
     }
 
     private static class NetworkService implements Runnable {
@@ -27,17 +31,25 @@ public class ExecutorService {
         public void run() { // run the service
             try {
                 for (;;) {
-                    pool.execute(new Handler(serverSocket.accept()));
+//                    pool.execute(new Handler(serverSocket.accept()));
+                    var t = pool.submit(new Handler(serverSocket.accept()) {
+
+                    });
+                    System.out.println(t.get());
                 }
             } catch (IOException ex) {
                 pool.shutdown();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
             }
         }
     }
 
     private  static class Handler implements Runnable {
         private final Socket socket;
+
         Handler(Socket socket) { this.socket = socket; }
+
         public void run() {
             // read and service request on socket
             System.out.println("==== Client Connected ====");
