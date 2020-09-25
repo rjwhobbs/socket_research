fde56cfc06902567f66fbb2cb471aae18bac14d4
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-25 22:47:23 +0200

    testing out Async server with future approach

--- chat-server/src/main/java/pmalope/AsyncTestServer.java
+++ chat-server/src/main/java/pmalope/AsyncTestServer.java
@@ -1,13 +1,38 @@
 package pmalope;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 /*
 *
 * Ref: https://www.baeldung.com/java-nio-2-async-channels
 * */
 public class AsyncTestServer {
+
+    public static void main(String[] args) {
+        new AsyncTestServer().futureApproach();
+    }
+
 //    TODO
     public void futureApproach() {
+        System.out.println("Testing out Future Approach...");
+        try {
+            AsynchronousServerSocketChannel server
+                    = AsynchronousServerSocketChannel.open().bind(new InetSocketAddress("localhost", 5000));
+            System.out.println("Waiting for connection...");
+            Future<AsynchronousSocketChannel> future = server.accept();
 
+            AsynchronousSocketChannel client = future.get();
+            System.out.println("Connection Accepted");
+            System.out.println("Client IP address" + client.getLocalAddress());
+            System.out.println("Debug " + future.isDone());
+
+        } catch (IOException | ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 //    TODO
