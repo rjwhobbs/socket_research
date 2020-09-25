e63dad7c58f907a47edce3fd43840a2c4e7a0ed3
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-25 16:38:06 +0200

    Added TODOs

---
+++ chat-server/src/main/java/pmalope/AsyncTestServer.java
@@ -0,0 +1,17 @@
+package pmalope;
+
+/*
+*
+* Ref: https://www.baeldung.com/java-nio-2-async-channels
+* */
+public class AsyncTestServer {
+//    TODO
+    public void futureApproach() {
+
+    }
+
+//    TODO
+    public void completionHandlerApproach() {
+
+    }
+}
