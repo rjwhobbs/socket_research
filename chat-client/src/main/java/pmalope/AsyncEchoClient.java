1bf7e1ff178f6bac3e36534637b2ad55b7d08f3b
martian1431 <pmalope@student.wethinkcode.co.za>
2020-09-26 16:19:25 +0200

    There's a bug when I send message from client to server. The client is not reading the entire response from the server.

--- chat-client/src/main/java/pmalope/AsyncEchoClient.java
+++ chat-client/src/main/java/pmalope/AsyncEchoClient.java
@@ -4,38 +4,78 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class AsyncEchoClient {
-    private  static final int PORT = 5000;
-    private  static final String HOST = "localhost";
+    private static AsynchronousSocketChannel clientChannel;
+    private static final int PORT = 5000;
+    private static final String HOST = "localhost";
+
     public static void main(String[] args) throws IOException {
-        try (
-                Socket echoSocket = new Socket(HOST, PORT);
-                PrintWriter out =
-                        new PrintWriter(echoSocket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(echoSocket.getInputStream())
-                        );
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
-        ) {
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println(":::DEBUG:::Server response ");
-                System.out.println(in.readLine());
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            /*
+            * An asynchronous channel for stream-oriented connecting sockets.
+            * */
+            clientChannel = AsynchronousSocketChannel.open();
+            InetSocketAddress hostAddress = new InetSocketAddress("localhost", PORT);
+            /*
+            * Establish connection
+            * */
+            Future<Void> future = clientChannel.connect(hostAddress);
+            /*
+            * Await connection
+            * */
+            future.get();
+            System.out.println(":::DEBUG::: connected to server - " + clientChannel.getRemoteAddress());
+            System.out.println("Enter your message or type 'bye' to quit");
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine().trim();
+                if (input.toLowerCase().equals("bye")) {
+                    System.out.println("Thank you for trying");
+                    System.exit(-1);
+                } else if (input.isEmpty()) {
+                    System.out.println(":::ERROR::: please enter your message or type bye to quit");
+                } else {
+                    String response = sendMessage(input);
+                    System.out.println(response);
+                }
+//                System.out.println(input);
             }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + HOST);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    HOST);
-            System.exit(1);
+        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
+    }
+
+    public static String sendMessage(String message) throws ExecutionException, InterruptedException {
+        byte[] byteMsg = message.getBytes();
+        ByteBuffer buffer = ByteBuffer.wrap(byteMsg);
+        Future<Integer> writeResult = clientChannel.write(buffer);
+
+        // do some computation
+        System.out.println(":::DEBUG::: Do some computation on writeResults");
+
+        writeResult.get();
+        buffer.flip();
+        Future<Integer> readResult = clientChannel.read(buffer);
+
+        // do some computation
+        System.out.println(":::DEBUG::: Do some computation on readResults");
+
+        readResult.get();
+        String echo = new String(buffer.array()).trim();
+        buffer.clear();
+        return echo;
     }
 }
