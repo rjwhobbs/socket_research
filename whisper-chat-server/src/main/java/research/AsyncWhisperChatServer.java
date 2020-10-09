c1ce4986cf971d16556995c723ce2d94729aada6
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-10-09 15:49:02 +0200

    Added hashmaps to store ref to object that stores a ref to the client's channel

--- whisper-chat-server/src/main/java/research/AsyncWhisperChatServer.java
+++ whisper-chat-server/src/main/java/research/AsyncWhisperChatServer.java
@@ -4,16 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+class ClientAttachment {
+  ByteBuffer buffer = ByteBuffer.allocate(4096);
+  AsynchronousSocketChannel client;
+
+  ClientAttachment(AsynchronousSocketChannel client) {
+    this.client = client;
+  }
+}
 
 public class AsyncWhisperChatServer {
 
   private static BufferedReader blockerReader = new BufferedReader(new InputStreamReader(System.in));
 
-//  private List<Client> brokerTable;
-//  private List<Client> marketTable;
+  private HashMap<String, ClientAttachment> brokers = new HashMap<>();
+  private HashMap<String, ClientAttachment> markets = new HashMap<>();
+  private static int brokersIndex = 1;
+  private static int marketsIndex = 1;
 
   public void acceptBroker() {
     try (final AsynchronousServerSocketChannel brokerChannel = AsynchronousServerSocketChannel.open()) {
@@ -25,13 +39,31 @@ public class AsyncWhisperChatServer {
           public void completed(AsynchronousSocketChannel result, Object attachment) {
             if (result.isOpen()) {
               brokerChannel.accept(null, this);
+              try {
+                registerBroker(result);
+              } catch (ExecutionException e) {
+                System.out.println("Router Error in acceptBroker(): ");
+                e.printStackTrace();
+              } catch (InterruptedException e) {
+                System.out.println("Router Error in acceptBroker(): ");
+                e.printStackTrace();
+              }
               System.out.println("Broker Connected");
             }
           }
 
+          private void registerBroker(AsynchronousSocketChannel client) throws ExecutionException, InterruptedException {
+            String brokerID = Integer.toString(brokersIndex);
+            ++brokersIndex;
+            client.write(ByteBuffer.wrap(brokerID.getBytes())).get();
+            ClientAttachment clientAttachment = new ClientAttachment(client);
+            brokers.put(brokerID, clientAttachment);
+            System.out.println(brokers.entrySet());
+          }
+
           @Override
           public void failed(Throwable exc, Object attachment) {
-
+            System.out.println("Router Error in acceptBroker(): " + exc.getMessage());
           }
         });
         System.out.println("Listening on port 5000");
@@ -41,10 +73,7 @@ public class AsyncWhisperChatServer {
       e.printStackTrace();
     }
   }
-
-  /**
-   *
-   */
+  
   public void acceptMarket() {
     try (final AsynchronousServerSocketChannel marketChannel = AsynchronousServerSocketChannel.open()) {
       InetSocketAddress hostAddress = new InetSocketAddress("localhost", 5001);
@@ -56,13 +85,31 @@ public class AsyncWhisperChatServer {
           public void completed(AsynchronousSocketChannel result, Object attachment) {
             if (result.isOpen()) {
               marketChannel.accept(null, this);
+              try {
+                registerMarket(result);
+              } catch (ExecutionException e) {
+                System.out.println("Router Error in acceptMarket(): ");
+                e.printStackTrace();
+              } catch (InterruptedException e) {
+                System.out.println("Router Error in acceptMarket(): ");
+                e.printStackTrace();
+              }
               System.out.println("Market Connected");
             }
           }
 
+          private void registerMarket(AsynchronousSocketChannel client) throws ExecutionException, InterruptedException {
+            String marketID = Integer.toString(marketsIndex);
+            ++marketsIndex;
+            client.write(ByteBuffer.wrap(marketID.getBytes())).get();
+            ClientAttachment clientAttachment = new ClientAttachment(client);
+            markets.put(marketID, clientAttachment);
+            System.out.println(markets.entrySet());
+          }
+
           @Override
           public void failed(Throwable exc, Object attachment) {
-            System.out.println("Something went wrong");
+            System.out.println("Router Error in acceptMarket(): " + exc.getMessage());
           }
         });
         System.out.println("Listening on port 5001");
