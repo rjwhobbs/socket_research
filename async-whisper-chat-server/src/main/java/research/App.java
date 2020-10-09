4dd09abd80331a4fb420478124f25f0cbdda6b22
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2020-10-09 11:40:55 +0200

    So we are using the std in method of blocking the while loop that contains the accept call, this works fine over if one types something on the std in the while loop will start again and this causes an \'AcceptPendingException\'. Made a blocker() method that doesn't cause the while loop to start again if input reader returns.

--- async-whisper-chat-server/src/main/java/research/App.java
+++ async-whisper-chat-server/src/main/java/research/App.java
@@ -2,6 +2,9 @@ package research;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello from server v2");
+    System.out.println("Hello from whisper chat.");
+    AsyncWhisperChatServer asyncWhisperChatServer = new AsyncWhisperChatServer();
+    asyncWhisperChatServer.acceptBroker();
+    asyncWhisperChatServer.acceptMarket();
   }
 }
--- async-whisper-chat-server/src/main/java/research/AsyncChatServerTwo.java
+++
@@ -1,5 +0,0 @@
-package research;
-
-public class AsyncChatServerTwo {
-
-}
